80967538e093b159553b2db3e1d7d5fceb631cba
Marat Ikhsanov <m.ihsanov2009@yandex.ru>
2022-07-13 11:16:16 +0300

    Delete estate directory

--- estate/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('users.urls', namespace='users')),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('', include('citizens.urls', namespace='citizens')),
-]
